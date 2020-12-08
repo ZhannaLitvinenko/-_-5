85418667dde0f17afb39ba956d79e04efa1c5620
ZhannaLitvinenko <zvlitvinenko01@gmail.com>
2020-12-08 17:13:24 +0200

    Виправила формулу Сімпсона.

--- Program.cs
+++ Program.cs
@@ -2,75 +2,70 @@
 
 namespace Lab5
 {
-	class Program
-	{
-		static double a, b;		
-		static int _2n;
+    class Program
+    {
+        static double a, b;
+        static int m;
 
-		static void Main(string[] args)
-		{
-			Input();
-			CalculateAverageVoltage();
+        static void Main(string[] args)
+        {
+            Input();
+            CalculateAverageVoltage();
 
-			Console.ReadKey();
-		}
+            Console.ReadKey();
+        }
 
-		static void Input()
-		{
-			Console.WriteLine("Введiть а:");
-			a = double.Parse(Console.ReadLine());
+        static void Input()
+        {
+            Console.WriteLine("Введiть а:");
+            a = double.Parse(Console.ReadLine());
 
-			Console.WriteLine("Введiть b:");
-			b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введiть b:");
+            b = double.Parse(Console.ReadLine());
 
-			Console.WriteLine("Введiть 2n:");
-			_2n = int.Parse(Console.ReadLine());
-		}
+            Console.WriteLine("Введiть кiлькiсть пiдiнтервалiв:");
+            m = int.Parse(Console.ReadLine());
+        }
 
-		static void CalculateAverageVoltage()
-		{
-			//формула середьного значення виглядає так:
-			//1/(b-a) * integral[a;b](125 * sin (wt / 3) - 30 * wt
-			//змінна - wt
+        static void CalculateAverageVoltage()
+        {
+            //формула середьного значення виглядає так:
+            //1/(b-a) * integral[a;b](125 * sin (wt / 3) - 30 * wt
 
-			//1. визначаємо крок інтегрування за формулою: h = (b - a) / 2n
-			double h = (b - a) / _2n;
-			//2. початкове значення wt, що підставляється у функцію = a
-			double wt = a;
+            //формула Сімпсона (для обрахунку визначеного інтегралу) 
+            //виглядає так: h / 3 * sum(f1, 4*f2, f3)
 
-			//3. формула Сімпсона (для обрахунку визначеного інтегралу) 
-			//виглядає так: h / 3 * sum(f1, 2*f2, 4*f3, 2*f4, 4*f5, ... , f[2n])
+            //кількість точок, що використовується
+            int s = 3,
+                //кількість вузлів
+                k = (s - 1) * m + 1;
+            //визначаємо крок інтегрування за формулою: h = |b - a| / (k - 1)
+            double h = Math.Abs(b - a) / (k - 1);
+            //початкове значення wt, що підставляється у функцію = a
+            double wt = a;
 
-			//спочатку рахуємо суму:
-			double sum = 0;
-			for(int i = 0; i < _2n; i++)
-			{
-				//обрахуємо поточне значення функції:
-				double currVoltage = MathHelper.CalculateVoltage(wt);
+            //спочатку рахуємо суму:
+            double integralValue = 0;
+            for (int i = 0; i < m; i++)
+            {
+                //h / 3 * (f0 + 4*f1 + f2)
+                double f0 = MathHelper.CalculateVoltage(wt),
+                    f1 = MathHelper.CalculateVoltage(wt + h),
+                    f2 = MathHelper.CalculateVoltage(wt + 2 * h);
 
-				//при першому або останньому і, додається значення функції:
-				if (i == 0 || i == _2n - 1)
-					sum += currVoltage;
-				//при парному і, додається значення функції, помножене на 2:
-				else if (i % 2 == 0)
-					sum += 2 * currVoltage;
-				//при непарному і, додається значення функції, помножене на 4:
-				else if (i % 2 != 0)
-					sum += 4 * currVoltage;
+                //додаємо обраховану частину до загальної суми:
+                integralValue += h / 3 * (f0 + 4 * f1 + f2);
 
-				//Поточна wt -- попереднє значення + крок(h):
-				wt += h;
-			}
+                //Наступна wt -- попереднє значення + 2 кроки:
+                wt += 2 * h;
+            }
 
-			//після обрахування суми, множимо на h/3, щоб отримати значення інтегралу:
-			double integralValue = (h / 3) * sum;
+            //щоб знайти середнє значення, домножаємо інтеграл на 1/(b-a):
+            double avgValue = 1 / (b - a) * integralValue;
 
-			//щоб знайти середнє значення, домножаємо інтеграл на 1/(b-a):
-			double avgValue = 1 / (b - a) * integralValue;
-
-			//Виводимо результат:
-			Console.WriteLine($"\nПри а = {a}, b = {b} та кiлькостi крокiв 2n = {_2n}," +
-				$" середнє значення напруги дорiвнює:\nY ср. = {Math.Round(avgValue, 5)}");
-		}
-	}
+            //Виводимо результат:
+            Console.WriteLine($"\nПри а = {a}, b = {b} та кiлькостi пiдiнтервалiв = {m}," +
+                $" середнє значення напруги дорiвнює:\nY ср. = {Math.Round(avgValue, 5)}");
+        }
+    }
 }
